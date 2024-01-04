ca919248435097de0f8f640554dbf2d34098b87a
Rk-0148 <103309385+Rk-0148@users.noreply.github.com>
2024-01-04 08:50:49 +0000

    updated files

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Accessing the children elements
+
 // console.log(document.body.firstChild)
 // console.log(document.body.lastChild)
 // let collect = console.log(document.body.childNodes)
@@ -7,24 +8,31 @@
 // console.log(document.body.childNodes[document.body.childNodes.length - 1])
 // console.log(document.body.hasChildNodes());
 
+// making array from HTML collection
+
 // let arr = Array.from(document.body.childNodes)
 // console.log(arr)
 // console.log(typeof arr)
 
 // // accessing the siblings and parents
-console.log(document.body.firstChild)
-console.log(document.body.firstElementChild)
-console.log(document.body.firstElementChild.nextSibling)
-console.log(document.documentElement.parentNode) // return the parent node
-console.log(document.documentElement.parentElement) // return parent tag
-console.log(document.getElementByClassName = "first");
+
+// console.log(document.body.firstChild)
+// console.log(document.body.firstElementChild)
+// console.log(document.body.firstElementChild.nextSibling)
+// console.log(document.documentElement.parentNode) // return the parent node
+// console.log(document.documentElement.parentElement) // return parent tag
+// console.log(document.getElementByClassName = "first");
 
 
 // console.log(document.body.firstElementChild);
 // console.log(document.body.firstElementChild.nextElementSibling);
 
-const changeBodyRed = () => {
-  document.body.firstElementChild.style.background = "red";
-  document.body.firstElementChild.style.color = "white";
-}
-changeBodyRed();
+// using js functions to change style of elements
+// const changeBodyRed = () => {
+//   document.body.firstElementChild.style.background = "red";
+//   document.body.firstElementChild.style.color = "white";
+// }
+// changeBodyRed();
+
+//
+
