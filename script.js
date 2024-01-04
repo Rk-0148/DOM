da336f81c1945b9f984d2636cf62688afe82e050
Rk-0148 <103309385+Rk-0148@users.noreply.github.com>
2024-01-04 14:06:36 +0000

    Updated files index and Script

--- script.js
+++ script.js
@@ -34,5 +34,15 @@
 // }
 // changeBodyRed();
 
-//
-
+// Table Navigation
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t);
+console.log(t.rows);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tFoot);
+console.log(t.tBodies);
+console.log(t.rows[0].rowIndex);
+console.log(t.rows[1].rowIndex);
+console.log(t.rows[2].rowIndex);
+console.log(t.rows.sectionRowIndex);
