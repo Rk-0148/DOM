1eb2df1fa6d9459636e704024623966eeed4934b
Rk-0148 <103309385+Rk-0148@users.noreply.github.com>
2024-01-04 15:23:19 +0000

    added DOM Searching in Js

--- script.js
+++ script.js
@@ -35,14 +35,42 @@
 // changeBodyRed();
 
 // Table Navigation
-let t = document.body.firstElementChild.firstElementChild;
-console.log(t);
-console.log(t.rows);
-console.log(t.caption);
-console.log(t.tHead);
-console.log(t.tFoot);
-console.log(t.tBodies);
-console.log(t.rows[0].rowIndex);
-console.log(t.rows[1].rowIndex);
-console.log(t.rows[2].rowIndex);
-console.log(t.rows.sectionRowIndex);
+// let t = document.body.firstElementChild.firstElementChild;
+// console.log(t);
+// console.log(t.rows);
+// console.log(t.caption);
+// console.log(t.tHead);
+// console.log(t.tFoot);
+// console.log(t.tBodies);
+// console.log(t.rows[0].rowIndex);
+// console.log(t.rows[1].rowIndex);
+// console.log(t.rows[2].rowIndex);
+// console.log(t.rows.sectionRowIndex);
+
+// Searching the DOM
+
+//1. getElementByID : it will be only single element coz ID is unique
+let home = document.getElementById("Home");
+home.style.color = "red";
+
+let cardtext = document.getElementsByClassName("card-text");
+cardtext[0].style.color = "pink",
+cardtext[0].style.background = "black";
+cardtext[1].style.color = "white";
+cardtext[1].style.background = "black";
+cardtext[2].style.color = "yellow";
+cardtext[2].style.background = "black";
+cardtext[3].style.color = "cyan";
+cardtext[3].style.background = "black";
+
+// 2. querySelectorAll : it will be collection of elements with that perticular class
+let ctitles = (document.querySelectorAll(".card-title"));
+ctitles[0].style.color = "red";
+ctitles[1].style.color = "blue";
+ctitles[2].style.color = "green";
+ctitles[3].style.color = "orange";
+
+document.querySelector(".this").style.background = "yellow";
+console.log(document.getElementsByTagName("a"));
+console.log(document.querySelector(".card").getElementsByTagName("a"));
+console.log(document.getElementsByName("search"));
